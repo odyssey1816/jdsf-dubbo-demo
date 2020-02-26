ff392142e0f1c1375655674a516359bca2074daa
liuzhipeng2 <liuzhipeng2@yy.com>
2020-02-26 18:37:11 +0800

    test

--- demo-server-2.7/src/main/java/com/jdcloud/jdsf/dubbo/demo/server/service/impl/TestServiceImpl.java
+++ demo-server-2.7/src/main/java/com/jdcloud/jdsf/dubbo/demo/server/service/impl/TestServiceImpl.java
@@ -1,6 +1,6 @@
 package com.jdcloud.jdsf.dubbo.demo.server.service.impl;
 
-import com.jdcloud.jdsf.dubbo.demo.server.service.TestService;
+import com.yy.aurogon.gumiho.verify.api.TestService;
 import org.apache.dubbo.config.annotation.Service;
 
 import java.util.Date;
@@ -9,16 +9,20 @@ import java.util.Date;
  * create by lishijun1 on 2019-08-01 21:30
  * email is lishijun1@jd.com
  */
-@Service(async = false)
+@Service
 public class TestServiceImpl implements TestService {
-
     @Override
-    public String echo(String value) {
-        return "Hello, "+ value;
+    public String hello(String input) {
+        return " haha====cccccc===="+input;
     }
 
-    @Override
-    public String echo(String value, long time) {
-        return "Hello, "+ value+" "+ new Date(time);
-    }
+//    @Override
+//    public String echo(String value) {
+//        return "Hello, "+ value;
+//    }
+//
+//    @Override
+//    public String echo(String value, long time) {
+//        return "Hello, "+ value+" "+ new Date(time);
+//    }
 }
---
+++ demo-server-2.7/src/main/java/com/yy/aurogon/gumiho/verify/api/TestService1.java
@@ -0,0 +1,5 @@
+package com.yy.aurogon.gumiho.verify.api;
+
+public interface TestService1 {
+    public String hello(String input);
+}
